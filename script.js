3e3a29573ba791d21305399e82d2ffcd94b43e4b
Hasib <moinuddinhasib@gmail.com>
2021-01-10 21:27:38 +0100

    Delay

--- script.js
+++ script.js
@@ -1,43 +1,49 @@
+  window.onload = caricaDati()
+  window.setInterval(caricaDati, 1000 )
 
-  window.onload = () => { caricaTable() };
-  setTimeout(() => {window.location.reload(true)},90000);
-  function caricaTable() {
-
+  function caricaDati() {
       $.ajax({
           url: "https://cors-anywhere.herokuapp.com/http://flight.apps.37.187.91.6.nip.io/flights",
           method: 'GET',
           dataType : "json",
-          success: function(dati){leggiDati(dati)}, 
+          success: function(resp){
+              if($("#corpo").html()===parsaDati(resp)){
+                  console.log("Dati sincronizzati")
+              }else{
+                  console.log("Aggiornamento dati")
+                  $("#corpo").html(parsaDati(resp))
+            }
+            }, 
           error: function(err){console.log(err)},
       });
 
   }
 
-  function leggiDati(dati) {
-      let dataHtml = ''
+  function parsaDati(dati) {
+      let dataHtml = ""
 
       for (let volo of dati) {
           dataHtml += `<tr>
       <td>${volo.id}</td>
       <td>${volo.code}</td>
-      <td>${volo.status}</br><button class='${volo.status}' onclick="cancel(${volo.id})"> Cancel </button></td>
-      <td>${volo.departure.date}</br>${volo.departure.airport}</td>
-      <td>${volo.arrival.date}</br>${volo.arrival.airport}</td>
+      <td>${volo.status}`;
+
+      if(volo.status==="SCHEDULED"){
+        dataHtml += `<br><button onclick="cancel(${volo.id})"> Cancel </button>`
+      }
+
+      dataHtml +=`</td><td>${volo.departure.date}<br>${volo.departure.airport}</td>
+      <td>${volo.arrival.date}<br>${volo.arrival.airport}</td>
       </tr>`;
       }
-      $("#corpo").html(dataHtml)
-
-      $(".CANCELLED").hide()
-      $(".ARRIVED").hide()
-      $(".FLYING").hide()
+      return dataHtml;
   }
 
   function cancel(id) {
       $.ajax({
           url: `https://cors-anywhere.herokuapp.com/http://flight.apps.37.187.91.6.nip.io/flights/${id}/cancel`,
           method: 'PUT',
-          success: () => {window.location.reload(true)}, 
+          success: function() {console.log(`Cancellato volo:${id}`)},
           error: function(err){console.error(err)},
-
       });
   }
